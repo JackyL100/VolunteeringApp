c9a9c460417f77395bdb75844eda48820ee3cf70
JackyL100 <the.axew.27@gmail.com>
2023-04-12 11:38:45 -0400

    Added cmd line check

--- client.cpp
+++ client.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 3) {
+        std::cout << "Hostname Port\n";
+        exit(0);
+    }
     std::cout << "Welcome to our Volunteering App!\n";
     std::cout << "You are able to create and view events on your calendar and also and view the events that you are attending.\n";
     std::cout << "Please sign/log in to continue";
-
+    
 
 }
