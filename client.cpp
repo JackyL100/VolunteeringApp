aa380e2ea696bfa8391f49e8070614a69cc8f6f3
JackyL100 <the.axew.27@gmail.com>
2023-04-12 11:58:36 -0400

    Checked cmd line arg

--- client.cpp
+++ client.cpp
@@ -9,6 +9,6 @@ int main(int argc, char* argv[])
     std::cout << "Welcome to our Volunteering App!\n";
     std::cout << "You are able to create and view events on your calendar and also and view the events that you are attending.\n";
     std::cout << "Please sign/log in to continue";
-    
+
 
 }
