f88255d3d0a5e06d8c6da523c4029292f0f45bee
Neill10 <95107962+Neill10@users.noreply.github.com>
2023-04-12 11:28:13 -0400

    basic layout for client login GUI.

---
+++ client.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to our Volunteering App!\n";
+    std::cout << "You are able to create and view events on your calendar and also and view the events that you are attending.\n";
+    std::cout << "Please sign/log in to continue";
+
+
+}
