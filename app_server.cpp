6e32c69ae85dbd7cdb30233b556b1cf072eb31e8
JackyL100 <the.axew.27@gmail.com>
2023-04-13 10:33:49 -0400

    Updated to kill server

--- app_server.cpp
+++ app_server.cpp
@@ -1,8 +1,14 @@
 #include "server.hpp"
 #include <chrono>
 
+void quit(Server& server) {
+    std::cin.get();
+    server.kill();
+}
+
 int main() {
     Server server(80, 10);
+    std::thread killThread(quit, server);
     while (server.isAlive()) {
         server.process_request(server.get_new_message(true));
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
